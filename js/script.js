c931d1b036c4373b7bc1617466fb3f8063a2fd0c
Giulia <giuliatullia93@gmail.com>
2021-06-14 22:15:04 +0200

    add-log

--- js/script.js
+++ js/script.js
@@ -5,14 +5,60 @@
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 var studente = {
-    nome : 'Giulia',
-    cognome : 'Fenu' ,
-    eta : '27' ,
+    'nome' : 'Giulia',
+    'cognome' : 'Fenu' ,
+    'eta' : '27' ,
+};
+
+// ciclo for-in con proprietà dell'oggetto
+for (var x in studente){
+    
+    console.log(studente[x]);
+}
+
+// Array di oggetti di studenti
+var arStudenti = [
+    {
+        'nome' : 'Bilbo',
+        'cognome' : 'Baggins' ,
+    },
+    {
+        'nome' : 'Ennio',
+        'cognome' : 'Annio' ,
+    },
+    {
+        'nome' : 'Ivo',
+        'cognome' : 'Avido' ,
+    }
+];
+
+var nameU = prompt('scrivi il tuo nome');
+var surname = prompt('scrivi il tuo cognome');
+var age =parseInt(prompt('scrivi la tua età'));
+var utente = {
+    'nome' : nameU,
+    'cognome' : surname ,
+    'eta' : age
 };
 
 
-for (var x in studente){
-    // document.getElementById("stampa").innerHTML = studente[x];
-    console.log(studente[x]);
+arStudenti.push(utente);
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (var i = 0; i < arStudenti.length; i++){
+    // console.log(arStudenti[i]);
+   for(var k in arStudenti[i]){
+       console.log(arStudenti[i][k]);
+   }
+   
 }
+
+
+
+
+
+
+
+
 
